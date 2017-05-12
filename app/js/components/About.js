b43bd24cc3ff6f661aae4e0d1ba1b73cf8feefa8
dj51234 <dj51234@yahoo.com>
2017-05-12 18:51:10 -0400

    fix type in about

--- app/js/components/About.js
+++ app/js/components/About.js
@@ -5,7 +5,7 @@ const About = function() {
     <div className="row">
       <div className="columns medium-8 small-centered">
         <h1 className="text-center">About</h1>
-        <p>This is a simple React weather app that leveragesthe OpenWeatherMap API along with a React app structure.  Here are a few links for resources:</p>
+        <p>This is a simple React weather app that leverages the OpenWeatherMap API along with a React app structure.  Here are a few links for resources:</p>
         <ol>
           <li><a href="https://facebook.github.io/react">React</a> - Javascript framework</li>
           <li><a href="http://openweathermap.org">OpenWeatherMap API</a> - Used for API to fetch weather</li>
